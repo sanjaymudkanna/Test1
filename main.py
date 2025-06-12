c0152d419c20d096891442ebabc7fcaf6b8b0c77
sanjay mudkanna <sanjay.mudkanna94@gmail.com>
2025-06-12 13:38:44 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def decor(func):
+	def inner(name):
+		if name=='Sunny':
+			print("Hello Sunny Bad Morning")
+
+		else:
+			func(name)
+	return inner
+
+@decor
+def wish(name):
+	print("Hello",name,"Good Morning")
+
+wish('John')
+
+wish('Ravi')
+
+wish('Sunny')
